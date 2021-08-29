001712ab3370cd300566932e174f4ec1f2dd89f9
Vadiraj Purohit <vadzz.java@gmail.com>
2021-08-28 21:01:10 -0400

    Initial Commit

--- src/main/java/com/projections/example/service/HelloService.java
+++ src/main/java/com/projections/example/service/HelloService.java
@@ -1,8 +1,6 @@
 package com.projections.example.service;
 
-import com.projections.example.entity.Department;
 import com.projections.example.model.DepartmentProjection;
-import com.projections.example.model.HelloResponse;
 import com.projections.example.repository.DepartmentRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -20,19 +18,11 @@ public class HelloService {
     private final DepartmentRepository departmentRepository;
 
     public List<DepartmentProjection> fetch() {
-
         return departmentRepository.findDepartmentByDepartmentId("ff8080817b8ece9b017b8eceb15e0000");
-
-
     }
 
     public List<DepartmentProjection> fetchAsync() {
-
         return supplyAsync(() -> departmentRepository.findDepartmentByDepartmentId("ff8080817b8ece9b017b8eceb15e0000"))
-                .thenApplyAsync(departments -> {
-                    return departments;
-                }).join();
-
-
+                .thenApplyAsync(departments -> departments).join();
     }
 }
